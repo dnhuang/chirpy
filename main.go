60e3efa7dcccfda412546f3739407c49cb9b5e36
dnhuang <dnhuang@berkeley.edu>
2024-02-14 21:25:01 -0800

    build fileserver

--- main.go
+++ main.go
@@ -6,8 +6,11 @@ import (
 )
 
 func main() {
+	const filePathRoot = "."
 	const port = "8080"
+
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
 	corsMux := middlewareCors(mux)
 
 	srv := &http.Server{
